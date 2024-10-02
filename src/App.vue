2497b9f3e26e8f1d2244ead4584ad85455196723
azulejocode <dev.sol.arias@gmail.com>
2024-10-02 00:20:01 -0500

    [base]

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://github.com/AzulejoCode" target="_blank">
+      <img src="./assets/azulejo_profile.webp" class="logo vue" alt="AzulejoCode logo"/>
+    </a>
+  </div>
+  <HelloWorld msg="Azulejo Code"/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+  border-radius: 50%;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <a class="button" target="_blank" href="https://github.com/AzulejoCode">Github Repository</a>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
